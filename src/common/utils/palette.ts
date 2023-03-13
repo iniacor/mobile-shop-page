9c84f230bc0caef18e4b1f37a03710901be47bf4
ihor <evroigor86@gmail.com>
2023-03-13 09:49:34 +0200

    add bundle optimisation

--- src/common/utils/palette.ts
+++
@@ -1,3 +0,0 @@
-import hexRgb from 'hex-rgb';
-
-export const getRgba = (hex: string, alpha = 1): string => hexRgb(hex, { format: 'css', alpha });
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,54 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
   const config = {
     entry: './src/index.tsx',
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      chunkFilename: isProduction ? '[name].[contenthash].chunk.js' : '[name].chunk.js',
       assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
       publicPath: '/',
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+        }),
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [
+                ['gifsicle', { interlaced: true }],
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 5 }],
+                ['svgo', { name: 'preset-default' }],
+              ],
+            },
+          },
+        }),
+      ],
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            enforce: true,
+            chunks: 'all',
+          },
+        },
+      },
     },
     module: {
       rules: [
@@ -29,6 +65,9 @@ module.exports = (env, argv) => {
         {
           test: /\.(png|jpg|jpeg|gif)$/i,
           type: 'asset/resource',
+          generator: {
+            filename: 'images/[name].[contenthash][ext]',
+          },
         },
         {
           test: /\.svg$/,
@@ -70,11 +109,9 @@ module.exports = (env, argv) => {
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
       alias: {
-        '@entities': path.resolve(__dirname, 'src/entities'),
         '@components': path.resolve(__dirname, 'src/components'),
         '@theme': path.resolve(__dirname, 'src/theme'),
         '@theme/*': path.resolve(__dirname, 'src/theme/*'),
-        '@common': path.resolve(__dirname, 'src/common'),
       },
     },
     devServer: {
