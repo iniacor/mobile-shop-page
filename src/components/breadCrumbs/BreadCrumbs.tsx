b2bb84bc4923ee7344b7853edee66ee48d3074c1
ihor <evroigor86@gmail.com>
2023-03-13 05:43:36 +0200

    make breadcrubs

--- src/components/breadCrumbs/BreadCrumbs.tsx
+++ src/components/breadCrumbs/BreadCrumbs.tsx
@@ -1,86 +1,75 @@
 import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 import * as Styled from './BreadCrumbs.styled';
-import { Link } from '@mui/material';
-import { useNavigate } from 'react-router';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
-export default function Breadcrumbs() {
-  const navigate = useNavigate();
+const breadcrumbNameMap = {
+  '/': 'Home',
+  '/electronics': 'Electronics',
+  '/electronics/:id': 'Product',
+};
 
-  const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    event.preventDefault();
-    navigate('/');
-  };
+function BreadcrumbItem({ to, ...rest }) {
+  const location = useLocation();
+  const pathnames = location.pathname.split('/').filter(x => x);
 
-  const breadcrumbs = [
-    <Link underline="hover" key="1" color="inherit" onClick={handleClick}>
-      Home
-    </Link>,
-    <Typography key="2" color="inherit">
-      Electronics
-    </Typography>,
-  ];
+  let isMatch = false;
+  const breadcrumbs = pathnames.map((_, index) => {
+    const url = `/${pathnames.slice(0, index + 1).join('/')}`;
+    const isActive = url === to;
+    const text = breadcrumbNameMap[url];
+    isMatch = isMatch || isActive;
+
+    return (
+      <Link
+        key={url}
+        color={isActive ? 'text.primary' : 'inherit'}
+        component={RouterLink}
+        to={url}
+        underline="hover"
+        {...rest}
+      >
+        {text}
+      </Link>
+    );
+  });
+
+  if (!isMatch) {
+    breadcrumbs.push(
+      <Typography key="not-found" color="text.primary">
+        Not Found
+      </Typography>,
+    );
+  }
+
+  return breadcrumbs;
+}
+
+function Breadcrumbs() {
+  const location = useLocation();
+  const pathnames = location.pathname.split('/').filter(x => x);
 
   return (
-    <Styled.Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-      {breadcrumbs}
+    <Styled.Breadcrumbs aria-label="breadcrumb">
+      <Link color="inherit" component={RouterLink} to="/">
+        Home
+      </Link>
+      {pathnames.map((value, index) => {
+        const url = `/${pathnames.slice(0, index + 1).join('/')}`;
+        const text = breadcrumbNameMap[url];
+        const isActive = url === location.pathname;
+
+        return isActive ? (
+          <Typography key={url} color="text.primary">
+            {text}
+          </Typography>
+        ) : (
+          <BreadcrumbItem key={url} to={url} />
+        );
+      })}
     </Styled.Breadcrumbs>
   );
 }
 
-// import * as React from 'react';
-// import { useRoutes, useLocation } from 'react-router-dom';
-// import * as Styled from './BreadCrumbs.styled';
-// import Typography from '@mui/material/Typography';
-// import Link from '@mui/material/Link';
-// import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-
-// function handleClick(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-//   event.preventDefault();
-//   console.info('You clicked a breadcrumb.');
-// }
-
-// export default function Breadcrumbs() {
-//   const location = useLocation();
-//   const routes = useRoutes([
-//     { path: '/', breadcrumb: 'Home' },
-//     { path: '/electronics', breadcrumb: 'Electronics' },
-//     { path: '/electronics/:id', breadcrumb: ({ params }) => `Product ${params.id}` },
-//   ]);
-//   console.log(routes.length);
-
-//   const pathnames = location.pathname.split('/').filter(x => x);
-//   const breadcrumbs = pathnames.map((_, index) => {
-//     const url = `/${pathnames.slice(0, index + 1).join('/')}`;
-//     const route = routes.find(r => r.path === url);
-//     return route && route.breadcrumb;
-//   });
-
-//   return (
-//     <Styled.Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-//       {breadcrumbs.map((breadcrumb, index) => {
-//         const route = routes[index];
-//         if (route) {
-//           return (
-//             <Link
-//               key={route.path}
-//               underline="hover"
-//               color={index === breadcrumbs.length - 1 ? 'text.primary' : 'inherit'}
-//               href={route.path}
-//               onClick={handleClick}
-//             >
-//               {breadcrumb}
-//             </Link>
-//           );
-//         } else {
-//           return (
-//             <Typography key={breadcrumb} color="text.primary">
-//               {breadcrumb}
-//             </Typography>
-//           );
-//         }
-//       })}
-//     </Styled.Breadcrumbs>
-//   );
-// }
+export default Breadcrumbs;
