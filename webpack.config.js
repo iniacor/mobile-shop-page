6523356db6dcfc55710a09f47acf1904f931025d
ihor <evroigor86@gmail.com>
2023-03-17 21:21:34 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
   const config = {
     entry: './src/index.tsx',
     output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'build'),
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
       publicPath: '/',
